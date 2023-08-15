8dfd04cb45746fc3fc8e1e0e9bbf07906c62cb06
Kevin Salazar <regatzert@gmail.com>
2023-08-14 23:30:27 -0500

    Primer merge: subida de archivos al repositorio

---
+++ src/test/java/com/crud/MysqlJPA/MysqlJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.MysqlJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
